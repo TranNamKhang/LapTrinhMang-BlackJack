3d52bddc4c74289ee6e87945aa74aba354e2d70f
NguyenXuanMinh22 <nguyenxuanminh289@gmail.com>
2025-08-17 16:20:41 +0700

    Chọn chip cược

--- BlackJackGame.Client/Views/BetWindow.xaml.cs
+++ BlackJackGame.Client/Views/BetWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System.Windows;
-using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 
-namespace BlackJackGame.Client.Views
+namespace BlackJackGame.Client
 {
     public partial class BetWindow : Window
     {
@@ -10,30 +12,44 @@ namespace BlackJackGame.Client.Views
         public BetWindow()
         {
             InitializeComponent();
+            Loaded += (s, e) => FadeIn();
+        }
+
+        private void FadeIn()
+        {
+            var anim = new DoubleAnimation(0, 1, TimeSpan.FromMilliseconds(180));
+            BeginAnimation(OpacityProperty, anim);
+            Pulse(this);
+        }
+
+        private void Pulse(FrameworkElement fe)
+        {
+            fe.RenderTransformOrigin = new Point(0.5, 0.5);
+            var st = new ScaleTransform(1, 1);
+            fe.RenderTransform = st;
+
+            var up = new DoubleAnimation(0.96, 1, TimeSpan.FromMilliseconds(220)) { EasingFunction = new BackEase() };
+            st.BeginAnimation(ScaleTransform.ScaleXProperty, up);
+            st.BeginAnimation(ScaleTransform.ScaleYProperty, up);
         }
 
         private void Chip_Click(object sender, RoutedEventArgs e)
         {
-            if (sender is Button btn)
+            if (sender is FrameworkElement fe && int.TryParse(fe.Tag?.ToString(), out int bet))
             {
-                if (int.TryParse(btn.Content.ToString(), out int bet))
-                {
-                    SelectedBet = bet;
-                    BetAmountText.Text = SelectedBet.ToString();
-                }
+                SelectedBet = bet;
             }
         }
 
         private void Confirm_Click(object sender, RoutedEventArgs e)
         {
-            if (SelectedBet > 0)
-            {
-                DialogResult = true;
-            }
-            else
-            {
-                MessageBox.Show("Please select a bet amount before confirming.");
-            }
+            if (SelectedBet > 0) { DialogResult = true; Close(); }
+            else MessageBox.Show("Vui lòng chọn số tiền cược!");
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false; Close();
         }
     }
 }
