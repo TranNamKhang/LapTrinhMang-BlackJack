b438e1a62d5ad5ccd100db323d327d7fa221e352
Vũ Mạnh Hùng <hungvm7175@ut.edu.vn>
2025-08-18 10:16:56 +0700

    Add file BackJackGame.cs

---
+++ BlackJackGame.Client/Models/BlackJackGame.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace BlackJackGame.Client.GameLogic
+{
+    public class BlackjackGame
+    {
+        public Deck Deck { get; private set; }
+        public Player Player { get; private set; }
+        public Dealer Dealer { get; private set; }
+
+        public BlackjackGame(string playerName)
+        {
+            Deck = new Deck();
+            Player = new Player { Name = playerName };
+            Dealer = new Dealer { Name = "Dealer" };
+        }
+
+        public void StartRound(int bet)
+        {
+            Player.ResetHand();
+            Dealer.ResetHand();
+            Deck = new Deck();
+
+            Player.Bet = bet;
+            Player.Balance -= bet;
+
+            Player.AddCard(Deck.DrawCard());
+            Player.AddCard(Deck.DrawCard());
+            Dealer.AddCard(Deck.DrawCard());
+            Dealer.AddCard(Deck.DrawCard());
+        }
+
+        public string DetermineWinner()
+        {
+            int playerScore = Player.GetScore();
+            int dealerScore = Dealer.GetScore();
+
+            if (playerScore > 21) return "Dealer wins!";
+            if (dealerScore > 21) { Player.Balance += Player.Bet * 2; return "Player wins!"; }
+            if (playerScore > dealerScore) { Player.Balance += Player.Bet * 2; return "Player wins!"; }
+            if (playerScore == dealerScore) { Player.Balance += Player.Bet; return "Draw!"; }
+            return "Dealer wins!";
+        }
+    }
+}
