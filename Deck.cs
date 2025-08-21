356cfaed6781126cc3cec3d91305a301d9903ccc
Khang <khangtn1454@ut.edu.vn>
2025-08-21 18:21:00 +0700

    code đã fix của Hùng
    add thêm code đã sửa

---
+++ Deck.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlackJackGame.Client.Models
+{
+    public class Deck
+    {
+        private List<Card> cards;
+        private Random random = new Random();
+
+        public Deck()
+        {
+            cards = new List<Card>();
+
+            foreach (CardSuit suit in Enum.GetValues(typeof(CardSuit)))
+            {
+                foreach (CardRank rank in Enum.GetValues(typeof(CardRank)))
+                {
+                    string suitSymbol = GetSuitSymbol(suit);
+                    string rankText = GetRankText(rank);     // "A", "2".."10", "J", "Q", "K"
+                    string rankWord = GetRankWord(rank);     // "ace", "2".."10", "jack", "queen", "king"
+                    int value = GetCardValue(rank);
+                    string suitLower = suit.ToString().ToLower();
+
+                    // Embedded resource pack URI matching csproj Resource includes under Controls/Images/Cards
+                    string imagePath = "pack://application:,,,/BlackJackGame.Client;component/Controls/Images/Cards/" +
+                                       rankWord + "_of_" + suitLower + ".png";
+
+                    cards.Add(new Card(suitSymbol, rankText, value, imagePath));
+                }
+            }
+        }
+
+        private string GetSuitSymbol(CardSuit suit)
+        {
+            switch (suit)
+            {
+                case CardSuit.Hearts: return "♥";
+                case CardSuit.Diamonds: return "♦";
+                case CardSuit.Clubs: return "♣";
+                case CardSuit.Spades: return "♠";
+                default: return "?";
+            }
+        }
+
+        private string GetRankText(CardRank rank)
+        {
+            switch (rank)
+            {
+                case CardRank.A: return "A";
+                case CardRank._2: return "2";
+                case CardRank._3: return "3";
+                case CardRank._4: return "4";
+                case CardRank._5: return "5";
+                case CardRank._6: return "6";
+                case CardRank._7: return "7";
+                case CardRank._8: return "8";
+                case CardRank._9: return "9";
+                case CardRank._10: return "10";
+                case CardRank.J: return "J";
+                case CardRank.Q: return "Q";
+                case CardRank.K: return "K";
+                default: return "?";
+            }
+        }
+
+        private int GetCardValue(CardRank rank)
+        {
+            switch (rank)
+            {
+                case CardRank.A: return 11;
+                case CardRank.J:
+                case CardRank.Q:
+                case CardRank.K:
+                case CardRank._10: return 10;
+                default: return (int)rank;
+            }
+        }
+
+        private string GetRankWord(CardRank rank)
+        {
+            switch (rank)
+            {
+                case CardRank.A: return "ace";
+                case CardRank.J: return "jack";
+                case CardRank.Q: return "queen";
+                case CardRank.K: return "king";
+                default: return ((int)rank).ToString();
+            }
+        }
+
+        public Card DrawCard()
+        {
+            if (cards.Count == 0) return null;
+            int index = random.Next(cards.Count);
+            Card card = cards[index];
+            cards.RemoveAt(index);
+            return card;
+        }
+
+        public int Count
+        {
+            get { return cards.Count; }
+        }
+    }
+}
