e1647452594154b37ec36acf554cc9b18f844785
Khang <khangtn1454@ut.edu.vn>
2025-08-21 17:57:22 +0700

    Add files via upload
    Fix code Hùng 21/8

---
+++ LeaderboardWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace BlackJackGame.Client
+{
+    public partial class LeaderboardWindow : Window
+    {
+        public LeaderboardWindow(List<LeaderboardEntry> entries)
+        {
+            InitializeComponent();
+            // If entries provided by caller, use them; otherwise, load from service
+            if (entries != null && entries.Count > 0)
+            {
+                LeaderboardGrid.ItemsSource = entries;
+            }
+            else
+            {
+                LoadLeaderboard();
+            }
+        }
+
+        private void LoadLeaderboard()
+        {
+            List<LeaderboardEntry> leaderboard = LeaderboardService.Load();
+            LeaderboardGrid.ItemsSource = leaderboard;
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
